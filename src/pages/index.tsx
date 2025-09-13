2447b13116993cca86120de220d5b21aadce0cf9
chendidida500-create <chendidida500@gmail.com>
2025-09-13 20:28:42 +0800

    Add home page component

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to Umi.js Example Project</h1>
+      <p>This is a basic example page created with Umi.js</p>
+      <p>Visit the <a href="/about">About</a> page to learn more.</p>
+    </div>
+  );
+};
+
+export default HomePage;
