2186cf28bf601699e6e2f6a98dd5bcae5a39c073
chendidida500-create <chendidida500@gmail.com>
2025-09-13 20:28:54 +0800

    Add about page component

---
+++ src/pages/about.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const AboutPage: React.FC = () => {
+  return (
+    <div>
+      <h1>About This Project</h1>
+      <p>This is an example Umi.js project created using GitHub API tools.</p>
+      <p>It demonstrates how to:</p>
+      <ul>
+        <li>Create a GitHub repository programmatically</li>
+        <li>Add files to the repository</li>
+        <li>Set up a basic Umi.js project structure</li>
+      </ul>
+      <a href="/">Back to Home</a>
+    </div>
+  );
+};
+
+export default AboutPage;
